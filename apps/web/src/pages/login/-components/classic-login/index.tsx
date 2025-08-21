eefaf5c8fbeeaf10f857ebc71e7c829cc592b465
fsj930210 <fsj930210@163.com>
2025-08-22 00:06:36 +0800

    fix: login page padding

--- apps/web/src/pages/login/-components/classic-login/index.tsx
+++ apps/web/src/pages/login/-components/classic-login/index.tsx
@@ -3,18 +3,23 @@ import type React from "react";
 import LoginImage from "@/assets/images/login-image.svg?react";
 import { LoginForm } from "../login-form";
 
-export function ClassicLogin({ className, ...props }: React.ComponentPropsWithoutRef<"div">) {
-	return (
-		<div className={cn("grid min-h-svh lg:grid-cols-2 p-4", className)} {...props}>
-			<div className="flex-center">
-				<div className="w-full max-w-sm space-y-6">
-					<h1 className=" flex-col-center text-2xl font-bold">Welcome to login</h1>
-					<LoginForm />
-				</div>
-			</div>
-			<div className="bg-muted relative hidden lg:flex-center">
-				<LoginImage />
-			</div>
-		</div>
-	);
+export function ClassicLogin({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<"div">) {
+  return (
+    <div className={cn("grid min-h-svh lg:grid-cols-2", className)} {...props}>
+      <div className="flex-center p-4">
+        <div className="w-full max-w-sm space-y-6">
+          <h1 className=" flex-col-center text-2xl font-bold">
+            Welcome to login
+          </h1>
+          <LoginForm />
+        </div>
+      </div>
+      <div className="bg-muted relative hidden lg:flex-center">
+        <LoginImage />
+      </div>
+    </div>
+  );
 }
